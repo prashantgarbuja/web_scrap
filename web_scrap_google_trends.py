bb5a48bc737b9d61ace6db5bad57735172f33615
Prashant Garbuja <prashantmaha@yahoo.com>
2023-10-05 06:50:25 +1100

    Update web_scrap_google_trends.py

--- web_scrap_google_trends.py
+++ web_scrap_google_trends.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import csv
-import os
 
 # Set up Chrome options with web browser pop-up disabled
 chrome_options = Options()
@@ -37,62 +36,20 @@ searches = soup.find_all('div', class_='search-count-title')
 file_name = 'GoogleTrends.csv'
 
 #os.makedirs(os.path.dirname(file_path), exist_ok=True)
-#Web Scrapping Top Google trends in Australia
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-import csv
-import os
 
-# Set up Chrome options with web browser pop-up disabled
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu') 
-
-# URL to scrape top daily trends in Australia
-url = "https://trends.google.com/trends/trendingsearches/daily?geo=AU&hl=en-US"
-driver = webdriver.Chrome(options=chrome_options) 
-
-driver.get(url)
-
-# Wait for the elements to be present
-wait = WebDriverWait(driver, 10)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'details-top')))
-
-page_source = driver.page_source
-
-# Close the Selenium WebDriver
-driver.quit()  
-
-soup = BeautifulSoup(page_source, 'html.parser')
-
-trending_topics = soup.find_all('div', class_='details-top')
-
-searches = soup.find_all('div', class_='search-count-title')
-
-file_name = 'GoogleTrends.csv'
-
-#os.makedirs(os.path.dirname(file_path), exist_ok=True)
-try:
-    print(os.getcwd())
-    with open(file_name, 'w',
-                newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['Google Top Trends in Australia'])
-        csvwriter.writerow(
-                ['Rank',
-                'Trending Topic', 
-                'Views']
+with open(file_name, 'w',
+            newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['Google Top Trends in Australia'])
+    csvwriter.writerow(
+            ['Rank',
+             'Trending Topic', 
+            'Views']
+            )
+    
+    for index, (trending_topic, search) in enumerate(zip(trending_topics, searches), start=1):
+            csvwriter.writerow(
+                [index, 
+                trending_topic.text.strip(), 
+                search.text.strip()]
                 )
-        
-        for index, (trending_topic, search) in enumerate(zip(trending_topics, searches), start=1):
-                csvwriter.writerow(
-                    [index, 
-                    trending_topic.text.strip(), 
-                    search.text.strip()]
-                    )
-except Exception as e:
-    print(f"An error occurred: {e}")
